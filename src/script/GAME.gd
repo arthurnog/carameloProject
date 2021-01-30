75fd8dceab98d9ea505a759cb22d3652eea4b912
arthurnog <a213293@dac.unicamp.br>
2021-01-30 20:12:07 -0300

    visual pronto

--- src/script/GAME.gd
+++ src/script/GAME.gd
@@ -9,6 +9,7 @@ func _input(event):
 	if Input.is_action_just_pressed("click"):
 		emit_signal("START")
 		set_process_input(false)
+		$Sprite.visible = false
 		
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
@@ -18,12 +19,13 @@ func _input(event):
 func _on_dog_dead():
 	#GAME OVER
 	#cena de derrota
-	queue_free()
+	get_tree().change_scene("res://src/scene/lost.tscn")
 	pass # Replace with function body.
 
 
 func _on_dog_win():
 	#WIN
 	#aqui a cena muda para a cena de vitória
+	get_tree().change_scene("res://src/scene/win.tscn")
 	queue_free()
 	pass # Replace with function body.
